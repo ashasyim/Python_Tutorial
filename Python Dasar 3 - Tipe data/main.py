17c33a6907564853c6c54325ffaad4e1826b715a
ashasyim <aristaley3@gmail.com>
2023-06-16 16:17:10 +0700

    Adding new exercise

---
+++ Python Dasar 3 - Tipe data/main.py
@@ -0,0 +1,54 @@
+# variabel tips
+
+# assignment satu baris
+# x, y, z = 1, 2, 3
+
+# memasukan nilai yang sama ke variabel yang berbeda
+# x = y = z = 0
+
+# umur = 18
+
+# variasi print
+# print("kamu berumur " + str(umur) + " tahun")
+# print("kalian berumur", umur, "tahun")
+# print(f"saya berumrur {umur} tahun")
+
+# INTEGER
+
+# umur = 18
+# score = 29
+# pemain = 5
+
+# print(f"kamu berumur {umur} tahun")
+# print(f"Payah! score kamu = {score}")
+# print(f"hanya {pemain} orang yang online!")
+
+# FLOAT
+
+# koordinat = 4434.344
+# harga = 20.99
+# pi = 3.14
+
+# print(f"lokasi kamu berada di {koordinat}")
+# print(f"kamu membeli baju dengan harga ${harga}")
+# print(f"pi = {pi}")
+
+# STRING
+
+# nama = "gatot kaca"
+# negara = 'indonesia'
+# email = "mlbb@landofdawn.com"
+
+# print(f"nama saya {nama}")
+# print(f"saya berasal dari {negara}")
+# print(f"hubungi saya lebih lanjut: {email}")
+
+# BOOL
+
+hujan = False
+online = True
+
+if hujan:
+    print("Bawa payung!")
+else:
+    print("Pake Topi!")
